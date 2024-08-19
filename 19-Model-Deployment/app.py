508dcd974824e714a92b7d9c92cc3a98ff9568db
gopimanimaran <mgopinathece@gmail.com>
2024-08-18 21:59:28 -0500

    Initial pull request

---
+++ 19-Model-Deployment/app.py
@@ -0,0 +1,31 @@
+############################
+######## IMPORTS ##########
+##########################
+from flask import Flask, request, jsonify
+import joblib
+import pandas as pd
+
+# Create Flask App
+app = Flask(__name__)
+
+
+# Create API routing call
+@app.route('/predict', methods=['POST'])
+def predict():
+
+    # Get JSON Request
+    feat_data = request.json
+    # Convert JSON request to Pandas DataFrame
+    df = pd.DataFrame(feat_data)
+    # Match Column Names
+    model = joblib.load("final_model.pkl")
+    col_names = joblib.load("column_names.pkl")
+    df = df.reindex(columns=col_names)
+    # Get prediction
+    prediction = list(model.predict(df))
+    # Return JSON version of Prediction
+    return jsonify({'prediction': str(prediction)})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
